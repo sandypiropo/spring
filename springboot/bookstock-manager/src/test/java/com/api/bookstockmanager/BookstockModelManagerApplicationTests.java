4408eac0bc25225b640fe19987ecc558fcfd44c2
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-09-21 23:33:20 -0300

    new project

---
+++ springboot/bookstock-manager/src/test/java/com/api/bookstockmanager/BookstockModelManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.bookstockmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstockModelManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
